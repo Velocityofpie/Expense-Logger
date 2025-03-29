c239e7e8986953ae24977c92729ffcdf723519c1
Velocityofpie <joshuamuq@gmail.com>
2025-03-28 22:08:11 -0400

    ts

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -8,5 +8,8 @@ for file in /usr/share/nginx/html/static/js/*.js; do
   fi
 done
 
+# Process nginx config templates
+DOLLAR='$' envsubst < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
 # Start Nginx
 exec nginx -g "daemon off;"
